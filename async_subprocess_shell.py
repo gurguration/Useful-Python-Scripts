ee0dc6c26a03e7ecd1f5381dda43c5cc95c05fff
gurguration <g.zakareishvili@gmail.com>
2019-08-23 13:07:56 +0400

    run commands

---
+++ async_subprocess_shell.py
@@ -0,0 +1,31 @@
+import asyncio
+import platform
+
+# EventloopPolicy for windows compatibility
+if platform.system().startswith('Win'):
+    asyncio.set_event_loop_policy(
+        asyncio.WindowsProactorEventLoopPolicy())
+
+
+async def run(cmd):
+    proc = await asyncio.create_subprocess_shell(
+        cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE)
+
+    stdout, stderr = await proc.communicate()
+
+    print(f'[{cmd!r} exited with {proc.returncode}]')
+    if stdout:
+        print(f'[stdout]\n{stdout.decode()}')
+    if stderr:
+        print(f'[stderr]\n{stderr.decode()}')
+
+
+async def main():
+    await asyncio.gather(
+        run('dir'),
+        run('\?')
+    )
+
+asyncio.run(main())
